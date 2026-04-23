8b16970a0b55a6d85a9b5946bf989f211b285820
deshpandeshriharsh <deshpandeshriharsh@gmail.com>
2026-04-23 17:17:29 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,31 @@
+public class App {
+    public int balance;
+
+    public App(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        if (amount < balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void main(String args[]) {
+        App a = new App(5000);
+        a.deposit(800);
+        System.out.println("Balance After deposit of 800: " + a.getBalance());
+        a.withdraw(400);
+        System.out.println("Balance After withdraw of 400: " + a.getBalance());
+    }
+}
